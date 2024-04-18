c33172bd6916d73508d8a72d8dd2049028eaaeeb
Caesarqq <107705159+Caesarqq@users.noreply.github.com>
2024-04-18 23:31:20 +0300

    1
    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-layout row justify-center>
+      <v-dialog v-model="dialog" persistent max-width="500">
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on" class="mt-5">Загрузить изображение</v-btn>
+        </template>
+        <v-card>
+          <v-card-title class="headline">Хотите загрузить изображение?</v-card-title>
+          <v-card-text>Выбирете способ загрузки изображения:</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" flat @click="setUploadType('local')">Локально</v-btn>
+            <v-btn color="red darken-1" flat @click="setUploadType('url')">URL</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+    <image-uploader v-if="uploadType" :type="uploadType"></image-uploader>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageUploader from './components/ImageUploader.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ImageUploader
+  },
+  data () {
+    return {
+      dialog: false,
+      uploadType: null
+    }
+  },
+  methods: {
+    setUploadType(type) {
+      this.uploadType = type;
+      this.dialog = false;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ImageUploader.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container>
+    <canvas ref="canvas" @click="getPixelColor" @mousemove="getPixelColor"></canvas>
+    <div v-if="pixelInfo">
+      <p>Цвет: rgb({{ pixelInfo.color.r }}, {{ pixelInfo.color.g }}, {{ pixelInfo.color.b }})</p>
+      <p>Координаты: x: {{ pixelInfo.x }}, y: {{ pixelInfo.y }}</p>
+      <p>Размер изображения: ширина: {{ imageWidth }}px, высота: {{ imageHeight }}px</p>
+    </div>
+    <v-form v-if="type === 'local'">
+      <v-file-input label="Выберите файл" @change="uploadLocal"></v-file-input>
+    </v-form>
+    <v-form v-if="type === 'url'">
+      <v-text-field label="Введите URL изображения" v-model="imageUrl"></v-text-field>
+      <v-btn @click="uploadFromUrl">Загрузить</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String
+  },
+  data() {
+    return {
+      imageUrl: "",
+      pixelInfo: null,
+      imageWidth: 0,
+      imageHeight: 0
+    };
+  },
+  methods: {
+    drawImageToCanvas(image) {
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext('2d');
+      this.imageWidth = image.width;
+      this.imageHeight = image.height;
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(image, 0, 0);
+    },
+    getPixelColor(event) {
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext('2d');
+      const rect = canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      const pixel = ctx.getImageData(x, y, 1, 1).data;
+      this.pixelInfo = {
+        color: {
+          r: pixel[0],
+          g: pixel[1],
+          b: pixel[2]
+        },
+        x: x,
+        y: y
+      };
+    },
+    uploadLocal(file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const img = new Image();
+        img.onload = () => {
+          this.drawImageToCanvas(img);
+        };
+        img.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    uploadFromUrl() {
+      const img = new Image();
+      img.onload = () => {
+        this.drawImageToCanvas(img);
+      };
+      img.src = this.imageUrl;
+    }
+  }
+}
+</script>
