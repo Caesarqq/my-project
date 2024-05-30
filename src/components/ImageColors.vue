4cf8173ac7ec0f7f4e0c8518563b230e3302d9df
Caesarqq <aszxclkh@gmail.com>
2024-05-30 03:23:59 +0300

    3.1 and 2.6
    3.1 and 2.6

--- src/components/ImageColors.vue
+++ src/components/ImageColors.vue
@@ -1,26 +1,49 @@
 <template>
-    <div>
-      <v-btn :color="currentTool === 'hand' ? 'red' : 'blue'" @click="selectTool('hand')">Рука</v-btn>
-      <v-btn :color="currentTool === 'pipette' ? 'red' : 'blue'" @click="selectTool('pipette')">Пипетка</v-btn>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        currentTool: null
-      };
-    },
-    methods: {
-      selectTool(tool) {
-        this.currentTool = tool;
-      }
-    }
-  };
-  </script>
-  
-  <style>
+<div>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+        :color="currentTool === 'hand' ? 'red' : 'blue'"
+        @click="selectTool('hand')"
+      >
+        Рука
+      </v-btn>
+    </template>
+    <span>Перемещение изображение</span>
+  </v-tooltip>
 
-  </style>
-  
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+        :color="currentTool === 'pipette' ? 'red' : 'blue'"
+        @click="selectTool('pipette')"
+      >
+        Пипетка
+      </v-btn>
+    </template>
+    <span>Выбор цвета</span>
+  </v-tooltip>
+</div>
+</template>
+
+<script>
+export default {
+data() {
+  return {
+    currentTool: null
+  };
+},
+methods: {
+  selectTool(tool) {
+    this.currentTool = tool;
+  }
+}
+};
+</script>
+
+<style>
+</style>
--- src/components/ImagePixelSearch.vue
+++ src/components/ImagePixelSearch.vue
@@ -1,50 +1,50 @@
 <template>
-  <div>
-    <canvas ref="canvas" @click="getPixelColor" @mousemove="getPixelColor"></canvas>
-    <div v-if="pixelInfo">
-      <p>Цвет: rgb({{ pixelInfo.color.r }}, {{ pixelInfo.color.g }}, {{ pixelInfo.color.b }})</p>
-      <p>Координаты: x: {{ pixelInfo.x }}, y: {{ pixelInfo.y }}</p>
-      <p>Размер изображения: ширина: {{ imageWidth }}px, высота: {{ imageHeight }}px</p>
-    </div>
+<div>
+  <canvas ref="canvas" @click="getPixelColor" @mousemove="getPixelColor"></canvas>
+  <div v-if="pixelInfo" class="padding">
+    <p>Цвет: rgb({{ pixelInfo.color.r }}, {{ pixelInfo.color.g }}, {{ pixelInfo.color.b }})</p>
+    <p>Координаты: x: {{ pixelInfo.x }}, y: {{ pixelInfo.y }}</p>
+    <p>Размер изображения: ширина: {{ imageWidth }}px, высота: {{ imageHeight }}px</p>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  props: {
-    imageWidth: {
-      type: Number,
-      required: true
-    },
-    imageHeight: {
-      type: Number,
-      required: true
-    }
+props: {
+  imageWidth: {
+    type: Number,
+    required: true
   },
-  data() {
-    return {
-      pixelInfo: null
-    };
-  },
-  methods: {
-    getPixelColor(event) {
-      const canvas = this.$refs.canvas;
-      const ctx = canvas.getContext('2d');
-      const rect = canvas.getBoundingClientRect();
-      const x = Math.floor((event.clientX - rect.left) * (canvas.width / rect.width));
-      const y = Math.floor((event.clientY - rect.top) * (canvas.height / rect.height));
-      const pixel = ctx.getImageData(x, y, 1, 1).data;
-      this.pixelInfo = {
-        color: {
-          r: pixel[0],
-          g: pixel[1],
-          b: pixel[2]
-        },
-        x: x,
-        y: y
-      };
-    }
+  imageHeight: {
+    type: Number,
+    required: true
   }
+},
+data() {
+  return {
+    pixelInfo: null
+  };
+},
+methods: {
+  getPixelColor(event) {
+    const canvas = this.$refs.canvas;
+    const ctx = canvas.getContext('2d');
+    const rect = canvas.getBoundingClientRect();
+    const x = Math.floor((event.clientX - rect.left) * (canvas.width / rect.width));
+    const y = Math.floor((event.clientY - rect.top) * (canvas.height / rect.height));
+    const pixel = ctx.getImageData(x, y, 1, 1).data;
+    this.pixelInfo = {
+      color: {
+        r: pixel[0],
+        g: pixel[1],
+        b: pixel[2]
+      },
+      x: x,
+      y: y
+    };
+  }
+}
 };
 </script>
 
@@ -52,5 +52,8 @@ export default {
 canvas {
   width: 1500px;
   height: 100vh;
+}
+.padding{
+  padding-left: 50px;
 }
 </style>
